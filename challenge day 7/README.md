ada0c0fc063a4e0b34583d10cb844acfb1522e74
Riccardo <riccardo.andreatta@hotmail.com>
2020-06-27 03:52:47 +0100

    Challenge day 7 added

---
+++ challenge day 7/README.md
@@ -0,0 +1,11 @@
+# Challenge Day 7: Proper Noun Correction
+
+Proper nouns always begin with a capital letter, followed by small letters.
+
+Correct a given noun so that it fits this statement.
+
+### Example
+
+Given the string `pARiS`, the output should be `Paris`.
+
+Given the string `John`, the output should be `John`.
---
+++ challenge day 7/main.js
@@ -0,0 +1,39 @@
+function properNounCorrection(str) {
+    str = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + str.substr(1);
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('properNounCorrection()', () => {
+    it('adds proper noun capitalization', () => {
+        // arrange
+        const str = 'pARiS';
+        
+        // act
+        const result = properNounCorrection(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe('Paris');
+    });
+    
+     it('properly cased words are still correct', () => {
+        // arrange
+        const str = 'John';
+        
+        // act
+        const result = properNounCorrection(str);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe('John');
+    });
+});
